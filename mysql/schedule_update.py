9c182ba497db32d5502697204205e156ff2075ff
RooTBeaM <panya.sirikul@gmail.com>
2023-06-13 16:02:48 +0700

    add schedule run

---
+++ mysql/schedule_update.py
@@ -0,0 +1,15 @@
+import schedule
+import time
+from main import mode_0,mode_1,mode_4
+print(f'schedule process every 5 minutes was started.')
+start = time.time()
+def job():
+    mode_0()
+    mode_1()
+    mode_4()
+    print(f'schedule process every 5 minutes was started.')
+schedule.every(5).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
